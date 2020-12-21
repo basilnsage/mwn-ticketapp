f2dff0e64cdd7d94f94c2c602301e0ac055071d4
Joshua Scherschel <j.scherschel17@gmail.com>
2020-12-21 09:33:32 -0800

    add some type casting protections when reading users from DB

--- auth/db.go
+++ auth/db.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
@@ -65,13 +66,57 @@ func (uc userColl) Read(ctx context.Context, user users.User) ([]users.User, err
 		return nil, fmt.Errorf("mongo.Cursor.All: %v", err)
 	}
 	for _, result := range res {
+		email, hash, uid, err := parseUserRes(result)
+		if err != nil {
+			return nil, err
+		}
 		foundUsers = append(foundUsers, users.User{
-			Email: result["email"].(string),
-			Hash: result["hash"].(primitive.Binary).Data,
-			Uid: result["_id"],
+			Email: email,
+			Hash: hash,
+			Uid: uid,
 		})
 	}
 	return foundUsers, nil
+}
+
+func parseUserRes(res bson.M) (string, []byte, string, error) {
+	var email, uid string
+	var hash []byte
+
+	emailRes, ok := res["email"]
+	if !ok {
+		return "", nil, "", errors.New("did not required Email field from mongo result")
+	}
+	switch t := emailRes.(type) {
+	case string:
+		email = t
+	default:
+		return "", nil, "", errors.New("found Email field but could not cast it to string")
+	}
+
+	hashRes, ok := res["hash"]
+	if !ok {
+		return "", nil, "", errors.New("did not required Hash field from mongo result")
+	}
+	switch t := hashRes.(type) {
+	case primitive.Binary:
+		hash = t.Data
+	default:
+		return "", nil, "", errors.New("found Hash field but could not cast it to []byte")
+	}
+
+	uidRes, ok := res["_id"]
+	if !ok {
+		return "", nil, "", errors.New("did not required _id field from mongo result")
+	}
+	switch t := uidRes.(type) {
+	case primitive.ObjectID:
+		uid = t.Hex()
+	default:
+		return "", nil, "", errors.New("found _id field but could not cast it to string")
+	}
+
+	return email, hash, uid, nil
 }
 
 func (uc userColl) Write(ctx context.Context, user users.User) (interface{}, error) {
